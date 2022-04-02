e3226bb8f2632c5162da5cdd33d1767290bb1ee7
shohaira15 <102850601+shohaira15@users.noreply.github.com>
2022-04-02 12:35:09 +0500

    Add files via upload

---
+++ BM.sql
@@ -0,0 +1,9 @@
+INSERT INTO Information(NAME,Id,Address) VALUE('Paras',133,'MUET');
+
+INSERT INTO Acc_info(num,Acc,balance) VALUES(133,'Paras',30);
+
+DELETE FROM Information WHERE NAME='Aiman'&Id=028;
+
+SELECT * FROM Information;
+
+SELECT * FROM Acc_info;
